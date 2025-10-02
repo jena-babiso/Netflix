b7a74ff15b591bea6070e11b389e2153e4de5899
JenaBoy <jenababiso45@gmail.com>
2025-10-02 11:03:08 +0300

    first commit

---
+++ netflix-clone/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Netflix Clone</h1>
+        <p>This is a simple Netflix clone application.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
